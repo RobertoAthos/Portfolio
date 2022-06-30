118e9198051921e66e2553cd19451e4ce48a3520
Roberto Athos <robertoathos22@gmail.com>
2022-06-30 15:51:00 -0300

    emailjs2

--- src/Components/Forms.js
+++ src/Components/Forms.js
@@ -26,49 +26,21 @@ function Forms() {
           console.log(error.text);
         }
       );
-    e.target.reset();
   };
 
   return (
-    <div>
-      <form ref={form} onSubmit={sendEmail}>
-        <Fade bottom cascade>
-          <div className="flex flex-col text-center ">
-            <input
-              type={"text"}
-              placeholder={t("Name")}
-              className="p-4 w-96 outline-none border-[#141C3A] border-2  mb-6"
-              name="name"
-            />
-            <input
-              type={"email"}
-              placeholder={t("Email")}
-              className="p-4 w-96 outline-none border-[#141C3A] border-2 mb-6"
-              name="email"
-            />
-            <input
-              type={"tel"}
-              placeholder={t("Number")}
-              className="p-4 w-96 outline-none border-[#141C3A] border-2 mb-6"
-              name="number"
-            />
-            <textarea
-              rows={5}
-              cols={50}
-              placeholder={t("Text")}
-              className="p-4 w-96 outline-none border-[#141C3A] border-2 mb-6"
-              name="message"
-            />
-          </div>
-        </Fade>
-        <Fade bottom cascade>
-          <input
-            className="bg-blue-700 p-4 rounded-xl font-bold text-xl w-96 text-center text-white hover:bg-blue-500 transition duration-300 outline-none cursor-pointer"
-            value="send message"
-          />
-        </Fade>
-      </form>
-    </div>
+    <Fade bottom cascade>
+          
+      <form ref={form} onSubmit={sendEmail} className='flex flex-col '>
+          <label className='text-blue-700 text-2xl mb-4'>Name</label>
+          <input type="text" name="user_name" className=' p-4 shadow-xl w-56' />
+          <label className='text-blue-700 text-2xl mb-4'>Email</label>
+          <input type="email" name="user_email" className=' p-4 shadow-xl w-56' />
+          <label className='text-blue-700 text-2xl mb-4'>Message</label>
+          <textarea name="message" className='p-4 shadow-xl w-56' />
+          <input type="submit" value="Send"  className=' p-4 shadow-xl cursor-pointer w-56 bg-blue-700'/>
+     </form>
+    </Fade>
   );
 }
 
